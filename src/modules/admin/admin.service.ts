53247d59112c3ec44a3ac1dcab3acb947f3f2933
mayinuddin <mayinuddin.munna@gmail.com>
2025-02-06 19:54:21 +0600

    restart

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -3,25 +3,24 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllAdminService = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
   const andCondition: Prisma.AdminWhereInput[] = [];
+  const searchFields = ["name", "email"];
+
+  console.log(filterData);
 
   if (params.searchTerm) {
     andCondition.push({
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
+      OR: searchFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
+      })),
     });
+  }
+
+  if (filterData.email || filterData.contactNumber) {
   }
 
   const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
