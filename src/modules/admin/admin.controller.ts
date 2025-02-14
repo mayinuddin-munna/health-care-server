fbf5e54e91698fe50099f3a0df57f658c63fe23f
mayinuddin <mayinuddin.munna@gmail.com>
2025-02-15 00:37:35 +0600

    search keys name fixed

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,9 +1,27 @@
 import { Request, Response } from "express";
 import { AdminServices } from "./admin.service";
+import { ParsedQs } from "qs";
+
+const pick = (object: ParsedQs, keys: string[]) => {
+  const finalObj: never[] = [];
+  for (const key of keys) {
+    if (object && Object.hasOwnProperty.call(object, key)) {
+      finalObj[key] = object[key];
+    }
+  }
+  console.log(finalObj);
+  return finalObj;
+};
 
 const getAllAdminController = async (req: Request, res: Response) => {
   try {
-    const result = await AdminServices.getAllAdminService(req.query);
+    const filters = pick(req.query, [
+      "name",
+      "email",
+      "searchTerm",
+      "contactNumber",
+    ]);
+    const result = await AdminServices.getAllAdminService(filters);
 
     res.status(200).json({
       success: true,
